38da97ff4a996c6108856dbc8649482955dd7d1a
Eduardo Pontes <eduardo.pontes@fcamara.com.br>
2020-05-06 14:51:44 -0300

    Initial Commit

---
+++ covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  covid19
+//
+//  Created by Eduardo Pontes on 06/05/20.
+//  Copyright © 2020 Eduardo Pontes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ covid19Tests/covid19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  covid19Tests.swift
+//  covid19Tests
+//
+//  Created by Eduardo Pontes on 06/05/20.
+//  Copyright © 2020 Eduardo Pontes. All rights reserved.
+//
+
+import XCTest
+@testable import covid19
+
+class covid19Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
